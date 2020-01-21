6d2a30e3392dda0300839baa73a118234c637ed7
Anupam-Panda <talk2anupamcontai@gmail.com>
2020-01-21 17:28:14 +0530

    Update app.js

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ var hostname = os.hostname();
 var app = express()
 
 app.get('/', function(req, res) {
-  res.send('Hello world from  -> ' + hostname + '!     Hello App in ICP!!\n')
+  res.send('Hello world from  -> ' + hostname + '!     Hello App in OCP!!\n')
 })
 app.listen(8080, function() {//previously 8080
   console.log('Sample app is listening on port 8080.')
